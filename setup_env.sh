fde8b84072cc2d62aaf00bebb058888b49d41112
masslightsquared <jason.wheeler@aapc.com>
2025-03-01 12:15:47 -0500

    add verl install

--- setup_env.sh
+++ setup_env.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
+# Download the Miniconda installer script
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+# Make the installer executable
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+
+# Run the installer
+./Miniconda3-latest-Linux-x86_64.sh -b
+
 # Create the conda environment
-conda create --name unsloth_env \
+conda create \
     python=3.11 \
     pytorch-cuda=12.1 \
     pytorch cudatoolkit xformers -c pytorch -c nvidia -c xformers \
     -y
 
 # Activate the conda environment
-conda activate unsloth_env
+source ~/miniconda3/bin/activate
 
-# Install unsloth and other dependencies
-pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
+# Install dependencies
 pip install git+https://github.com/huggingface/trl.git@e95f9fb74a3c3647b86f251b7e230ec51c64b72b
 pip install --no-deps peft accelerate bitsandbytes
 pip install diffusers
 pip install git+https://github.com/AnswerDotAI/RAGatouille.git
 pip install vllm
 pip install --upgrade pillow
-pip install wandb
+pip install flash-attn --no-build-isolation
+
+# Clone verl repository
+git clone https://github.com/volcengine/verl.git && cd verl && pip install -e .
